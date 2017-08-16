30d9e873953b7754692233ba399e7453d4c2c464
amandamc4 <amanda.mc4@hotmail.com>
2017-08-16 13:50:24 -0400

    Fixed typescript issue

--- MassasCantina/app/app.routing.js
+++ MassasCantina/app/app.routing.js
@@ -1,6 +1,7 @@
 "use strict";
-var router_1 = require('@angular/router');
-var home_component_1 = require('./components/home.component');
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@angular/router");
+var home_component_1 = require("./components/home.component");
 var appRoutes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: home_component_1.HomeComponent }
